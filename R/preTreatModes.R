9b9b369338f0cca46a7cc8de5d804028d4b30dc0
jasenfinch <jsf9@aber.ac.uk>
2018-08-31 03:56:18 +0100

    added MetaboProfile method for preTreatModes

--- R/preTreatModes.R
+++ R/preTreatModes.R
@@ -1,23 +1,53 @@
+
+preTreat <- function(dat,info,parameters){
+    
+    preTreated <- dat %>%
+        map(metabolyse,info = info,parameters = parameters)
+    preTreatedDat <- preTreated %>%
+        map(~{
+            .@preTreated$Data
+        }) %>%
+        bind_cols()
+    
+    preTreatedInf <- preTreated[[1]]@preTreated$Info
+    
+    all <- new('Analysis')
+    all@log <- list(packageVersion = packageVersion('metabolyseR'),analysis = date(),verbose = F)
+    all@parameters <- parameters
+    all@rawData <- list(Data = bind_cols(dat), Info = sampleInfo)
+    all@preTreated <- list(Data = preTreatedDat,Info = preTreatedInf)
+    
+    return(all)
+}
+
+
+
 #' preTreatModes
 #' @rdname preTreatModes
 #' @importFrom binneR binnedData info
 #' @importFrom metabolyseR metabolyse preTreatedData preTreatedInfo
 #' @importFrom dplyr bind_cols
 #' @importClassesFrom binneR Binalysis
+#' @importClassesFrom profilePro MetaboProfile
 
 setMethod('preTreatModes',signature = 'Binalysis',
-          function(binalysis,parameters){
-              dat <- binnedData(binalysis)
-              sampleInfo <- info(binalysis)
+          function(processedData,parameters){
+              dat <- binnedData(processedData)
+              sampleInfo <- info(processedData)
 
-              neg <- metabolyse(dat$n,sampleInfo,parameters)
-              pos <- metabolyse(dat$p,sampleInfo,parameters)
-
-              all <- new('Analysis')
-              all@log <- list(packageVersion = packageVersion('metabolyseR'),analysis = date(),verbose = F)
-              all@parameters <- parameters
-              all@rawData <- list(Data = bind_cols(dat$n,dat$p), Info = sampleInfo)
-              all@preTreated <- list(Data = bind_cols(preTreatedData(neg),preTreatedData(pos)),Info = preTreatedInfo(neg))
-
-              return(all)
+              preTreated <- preTreat(dat,sampleInfo,parameters)
+              
+              return(preTreated)
 })
+
+#' @rdname preTreatModes
+
+setMethod('preTreatModes',signature = 'MetaboProfile',
+          function(processedData,parameters){
+              dat <- processedData@Data
+              sampleInfo <- processedData@Info
+              
+              preTreated <- preTreat(dat,sampleInfo,parameters)
+              
+              return(preTreated)
+          })
