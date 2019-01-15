96ac8c7af0dc92a8269a6f6d09b03fd9c55ad7fa
jasenfinch <jsf9@aber.ac.uk>
2019-01-15 16:42:46 +0000

    added detect methods

--- R/filterCorrelations.R
+++ R/filterCorrelations.R
@@ -1,11 +1,24 @@
-#' preTreatModes
+#' filterCorrelations
 #' @export
-
-filterCorrelations <- function(correlations, rthresh = 0.7, n = 100000){
-    while (nrow(correlations) > n) {
-        correlations <- correlations %>%
-            filter(r > rthresh | r < -rthresh)
-        rthresh <- rthresh + 0.01
+filterCorrelations <- function(correlations, rthresh = 0.7, n = 100000, rIncrement = 0.01, nIncrement = 20000){
+    filCors <- function(cors,rthresh,n){
+        while (nrow(cors) > n) {
+            cat('rthresh = ',rthresh,'\n')
+            cors <- correlations %>%
+                filter(r > rthresh | r < -rthresh)
+            rthresh <- rthresh + rIncrement
+        }
+        return(cors)
     }
-    return(correlations)
+    
+    while (TRUE) {
+        cat('n = ',n,'\n')
+        cors <- filCors(correlations,rthresh,n)
+        if (nrow(cors) > 0) {
+            break()
+        } else {
+            n <- n + nIncrement
+        }
+    }
+    return(cors)
 }
