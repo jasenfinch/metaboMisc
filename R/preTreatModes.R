b4d68800a05a6c44302f0e787fca38212e015ef7
jasenfinch <jsf9@aber.ac.uk>
2019-11-21 13:29:09 +0000

    updated documentation. Improved console output of preTreatModes(). Bumped version to 0.2.2

--- R/preTreatModes.R
+++ R/preTreatModes.R
@@ -4,14 +4,53 @@
 #' @importFrom methods new
 #' @importFrom metabolyseR dat sinfo
 #' @importFrom metabolyseR analysisData analysisParameters
+#' @importFrom crayon blue red green
+#' @importFrom cli console_width
+#' @importFrom lubridate seconds_to_period
 
-preTreat <- function(dat,info,parameters,verbose = T){
+preTreat <- function(dat,info,parameters,verbose = TRUE){
     
     p <- analysisParameters('preTreat')
     p@preTreat <- parameters@preTreat
     
-    preTreated <- dat %>%
-        map(metabolyse,info = info,parameters = p,verbose = verbose)
+    if (verbose == TRUE) {
+        analysisStart <- date()
+        startTime <- proc.time()
+        version <- packageVersion('metabolyseR') %>%
+            as.character()
+        cat('\n',blue('metabolyseR'),' ',red(str_c('v',version)),' ',analysisStart,'\n',sep = '')
+        cat(rep('_',console_width()),'\n',sep = '')
+        print(p)
+        cat(rep('_',console_width()),'\n\n',sep = '')
+    }
+    
+    names(dat)[names(dat) == 'n'] <- 'Negative mode'
+    names(dat)[names(dat) == 'p'] <- 'Positive mode'
+    
+    preTreated <- names(dat) %>%
+        map(~{ 
+            m <- .
+            d <- dat[[m]]
+            
+            if (verbose == TRUE) {
+                startTime <- proc.time()
+                cat(blue(m),cli::symbol$continue,'\r',sep = '') 
+            }
+            
+            res <- metabolyse(d,info = info,parameters = p,verbose = FALSE)
+            
+            if (verbose == TRUE) {
+                endTime <- proc.time()
+                elapsed <- {endTime - startTime} %>%
+                    .[3] %>%
+                    round(1) %>%
+                    seconds_to_period() %>%
+                    str_c('[',.,']')
+                cat(blue(m),' \t\t',green(cli::symbol$tick),' ',elapsed,'\n',sep = '')
+            }
+            
+            return(res)
+        })
     preTreatedDat <- preTreated %>%
         map(~{
             .@preTreated %>%
@@ -27,6 +66,18 @@ preTreat <- function(dat,info,parameters,verbose = T){
     all@parameters <- parameters
     all@rawData <- analysisData(bind_cols(dat), info)
     all@preTreated <- analysisData(preTreatedDat,preTreatedInf)
+    
+    if (verbose == TRUE) {
+        endTime <- proc.time()
+        elapsed <- {endTime - startTime} %>%
+            .[3] %>%
+            round(1) %>%
+            seconds_to_period() %>%
+            str_c('[',.,']')
+        
+        cat(rep('_',console_width()),'\n',sep = '')
+        cat('\n',green('Complete! '),elapsed,'\n\n',sep = '')
+    }
     
     return(all)
 }
@@ -50,11 +101,11 @@ setMethod('preTreatModes',signature = 'Binalysis',
           function(processedData,parameters,verbose = T){
               dat <- binnedData(processedData)
               sampleInfo <- info(processedData)
-
+              
               preTreated <- preTreat(dat,sampleInfo,parameters,verbose = verbose)
               
               return(preTreated)
-})
+          })
 
 #' @rdname preTreatModes
 #' @export
