68e9b5cc118b57e39d0c613066db7434ad25edc3
jasenfinch <jsf9@aber.ac.uk>
2021-04-26 13:55:27 +0100

    added addAssignments with documentation and test

---
+++ R/add.R
@@ -0,0 +1,22 @@
+#' Add molecular formula assignments
+#' @rdname addAssignments
+#' @description Add molecular formaul assignments to an `Analysis` class object.
+#' @param analysis S4 object of class `Analysis`
+#' @param assignmwnt S4 object of class `Assignment`
+#' @return An S4 object of class `Analysis`
+#' @details Molecular formula assignments are added to the `Analysis` pre-treated data 
+#' @export
+
+setGeneric('addAssignments',
+           function(analysis,assignment)
+           standardGeneric('addAssignments'))
+
+#' @rdname addAssignments
+#' @importFrom MFassign assignedData
+
+setMethod('addAssignments',signature = c('Analysis','Assignment'),
+          function(analysis,assignment){
+              dat(analysis,'pre-treated') <- assignedData(assignment)   
+              
+              return(analysis)
+          })
---
+++ tests/testthat/test-add.R
@@ -0,0 +1,3 @@
+test_that("addAssignments works", {
+  added_assignemnts <- addAssignments(a,assignment)
+})
