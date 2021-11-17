65ffff67b9ddcd4cd8d0a5f44e40416268f87e90
jasenfinch <jsf9@aber.ac.uk>
2021-11-17 09:48:14 +0000

    sanitiseTable() now limits the number of characters allowed in strings

--- tests/testthat/test-sanitise.R
+++ tests/testthat/test-sanitise.R
@@ -2,9 +2,17 @@
 test_that("tables can be sanitised", {
     n_rows <- 10
     sig_figs <- 1
+    max_char <- 3
     
-    x <- sanitiseTable(iris,maxRows = n_rows,sigFig = sig_figs)
+    test_table <- iris %>% 
+        mutate(Species = as.character(Species))
+    
+    x <- sanitiseTable(test_table,
+                       maxRows = n_rows,
+                       sigFig = sig_figs,
+                       maxCharacters = max_char)
     
     expect_equal(nrow(x),n_rows)
     expect_equal(x$Sepal.Length[1],5)
+    expect_equal(max(nchar(x$Species)),max_char)
 })
