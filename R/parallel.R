5f8505c4de86dfb6218fa3b197d8b42555fe7747
jasenfinch <jsf9@aber.ac.uk>
2022-05-23 10:33:38 +0100

    allow user override of suitable parallel plan detection

--- R/parallel.R
+++ R/parallel.R
@@ -1,5 +1,7 @@
 #' Generate a suitable parallel processing plan
 #' @description Generate a suitable parallel processing future plan for your session. See `?future::plan` for more details on future plans.
+#' @param strategy Parallel strategy to use. `?future::plan` for details. If `NULL` (the default), an appropriate strategy will be detected.
+#' @param workers Number of workers to use. If `NULL`, the selected number will be two thirds of the available system workers.
 #' @examples 
 #' \dontrun{
 #' suitableParallelPlan()
@@ -7,14 +9,14 @@
 #' @importFrom future availableCores multisession multicore plan supportsMulticore
 #' @export
 
-suitableParallelPlan <- function(){
-    workers <- ceiling(availableCores() * 0.75)
+suitableParallelPlan <- function(strategy = NULL,workers = NULL){
     
-    if (supportsMulticore()) {
-        strategy <- 'multicore'
-    } else {
-        strategy <- 'multisession'
-    }
+    if (is.null(strategy)) strategy <- ifelse(
+        supportsMulticore(),
+        'multicore',
+        'multisession')
+    
+    if (is.null(workers)) workers <- ceiling(availableCores() * 0.75)
     
     message(paste('Setting up',strategy,'plan with',workers,'workers'))
     
